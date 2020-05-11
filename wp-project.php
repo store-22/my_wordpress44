52e7dacc99da593131335696243cbb7b775a8946
store-22 <64090712+store-22@users.noreply.github.com>
2020-05-11 15:23:38 +0530

    make it better

--- wp-project.php
+++ wp-project.php
@@ -1,10 +1,4 @@
-<html>
-<head>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-</head>
-<body>
+
 <?php
    $a = shell_exec( "git config --global user.name 2>&1");
    $e = array("saravana", "harsha");
@@ -17,38 +11,3 @@
    $d = shell_exec( "git config user.email 2>&1");
    print_r($d);
     ?>
-    <div class="jumbotron text-left">
-  <h1>Github Connection </h1>
-  <p><strong>Repository details</strong></p> 
-</div>
-  
-<div class="container">
-<table class="table table-striped">
-    <thead>
-      <tr>
-        <th>Github connection</th>
-        <th>details</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>Github username</td>
-        <td><?php echo $a; ?></td>
-      </tr>
-      <tr>
-        <td>Github Brach</td>
-        <td><?php echo $b; ?></td>
-      </tr>
-      <tr>
-        <td>Github URL</td>
-        <td><?php echo $c; ?></td>
-      </tr>
-      <tr>
-        <td>Github EMAIL</td>
-        <td><?php echo $d; ?></td>
-      </tr>
-    </tbody>
-  </table>
-</div>
-</body>
-</html>
