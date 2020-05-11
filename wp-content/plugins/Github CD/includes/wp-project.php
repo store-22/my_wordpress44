0c7ffc8914f53d23828b3fda5807d085b3063f7f
store-22 <64090712+store-22@users.noreply.github.com>
2020-05-11 19:01:26 +0530

    make it better

--- wp-content/plugins/Github CD/includes/wp-project.php
+++ wp-content/plugins/Github CD/includes/wp-project.php
@@ -1,10 +1,9 @@
 
 <?php
-   $a = shell_exec( "git config --global user.name 2>&1" );
-   $b = shell_exec( "git config --global branch.master.remote 2>&1");
-   $c = shell_exec( "git config --global remote.heroku.url 2>&1");
-   $d = shell_exec( "git config --global user.email 2>&1" );
-   print_r($d);
+   $a = shell_exec( "git config --system user.name 2>&1" );
+   $b = shell_exec( "git config --system branch.master.remote 2>&1");
+   $c = shell_exec( "git config --system remote.heroku.url 2>&1");
+   $d = shell_exec( "git config --system user.email 2>&1" );
    print_r($a);
    print_r($b);
    print_r($c);
