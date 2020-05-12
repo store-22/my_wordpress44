c2ddf4f26296d4e5ba1c1ee9ae4c4c11ebc328c2
store-22 <64090712+store-22@users.noreply.github.com>
2020-05-12 17:32:51 +0530

    make it better

--- wp-content/plugins/Github CD/includes/wp-project.php
+++ wp-content/plugins/Github CD/includes/wp-project.php
@@ -1,5 +1,5 @@
 <?php
-   $file = parse_ini_file("E:\make_it\my-wordpress44\.git\config");
+   $file = parse_ini_file(".git/config");
   print_r($file);
   print_r($file['url']);
   print_r($file['fetch']);
